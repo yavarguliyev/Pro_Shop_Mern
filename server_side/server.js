ec98e742102513ee8e02c562d39a55cd68215d33
yavarguliyev <yavarrg@code.edu.az>
2021-04-12 15:27:33 +0400

    cors policy modified

--- server_side/server.js
+++ server_side/server.js
@@ -29,8 +29,20 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json())
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // Website you wish to allow to connect
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
 });
 
@@ -65,12 +77,7 @@ app.use(
 app.use(xss())
 app.use(hpp())
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200,
-  methods: "GET, PUT, POST, DELETE"
-}
-app.use(cors(corsOptions))
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use('/api/v1/products', product)
 app.use('/api/v1/users', user)
